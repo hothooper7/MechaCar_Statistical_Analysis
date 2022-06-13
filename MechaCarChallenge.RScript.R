34592ca8b331e644933f2d795f0a1fab48871887
Aaron Shreve <aaronshreve@Aarons-MacBook-Pro.local>
2022-06-13 14:52:12 -0400

    Adding Suspension Analysis

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -9,3 +9,12 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #determine p-value and r-squared
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg))
+
+#import Suspension_Coil CSV
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#get the mean, median, variance, and standard deviation
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+#group each manufacturing lot by mean, median, variance, and standard deviation
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep') 
